2a74b7c061cdf4e0d1d114054f8a387da4ffa1c4
Kristina Lubinets <kris.lubinec@gmail.com>
2022-07-13 19:36:41 -0400

    Edit ex. 2 on async and callbacks

--- 7-11.js
+++ 7-11.js
@@ -85,8 +85,97 @@ setTimeout( function() {
 }, 5000)
 
 setInterval( function() { 
-  document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215);
 }, 5000)
 
 
 /*********************************************************** Exercise (3) *******************************************************************************/
+/*----------
+You are given the following list of movies
+
+Task 1
+Create a function called "showMovies" that
+- iterates through the "movies" array and
+- for each movie, it creates a <p> element with the movie title and director and append it to the #all-movies div.
+- it sets the innerText of the #movies-number element to the total number of the movies in the array "movies"
+
+Task 2
+Create a new function called "addMovie"
+- it receives a movie object as an argument - your can create a new object for your favorite movie following using the "myMovies" objects as a guide 
+- it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
+Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
+How many movies can you see on your page?
+
+Task 3
+Can you make sure the new movie you just added is showing on the screen? 
+TIP: use callbacks
+
+Task 4 - **Extra**
+- On the form with id #add-movies, add a "submit" event listener
+- When the button is clicked you need to:
+    - Read all input field values and create a new movie object literal with title, director, type and haveWatched properties
+    - Push the new object on the movies array 
+    - Display the updated list of movies on your page
+
+TIP: Use the functions you created on tasks 1-3
+================*/
+
+const movies = [
+  {
+    title: "Color Out of Space",
+    director: "Richard Stanley",
+    type: "sci-fi",
+    haveWatched: true,
+  },
+  {
+    title: "A Twelve-Year Night",
+    director: "Álvaro Brechner",
+    type: "horror",
+    haveWatched: false,
+  },
+  {
+    title: "The Whistlers",
+    director: "Corneliu Porumboiu",
+    type: "comedy",
+    haveWatched: true,
+  },
+  {
+    title: "The Invisible Man",
+    director: "Leigh Whannell",
+    type: "horror",
+    haveWatched: false,
+  },
+];
+
+// Task 1: create showMovies function
+const allMovies = document.getElementById('all-movies');
+setTimeout(showMovies, 1000);
+
+function showMovies() {
+  movies.forEach( el => {
+    const newP = document.createElement('p');
+    const textP = document.createTextNode(el.title + ' by_ ' + el.director);
+    newP.appendChild(textP);    
+    allMovies.appendChild(newP);
+  });
+}
+
+const moviesNumber = document.getElementById('movies-number').innerText = movies.length;
+
+// Task 2: create a new movie object for your favorite movie
+const myFavMovie = {
+    title: "OnePunchMan",
+    director: "some otaku",
+    type: "action",
+    haveWatched: true,
+}
+
+// Task 2-3: create addMovies function
+function addMovie(movie, callback) {
+  movies.push(myFavMovie);
+  document.body.style.backgroundColor = 'red';
+}
+
+setTimeout(addMovie(), 2000);    //new obj is added only if 'addMovie()', if 'addMvie' - doesn't add(but changes bg color)
+
+// Task 4 - **Extra**
